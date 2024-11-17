3f3601368b85f0f9a2ab320189514c466ffd3b9a
Kirlian Verus <127189354+Kyverus@users.noreply.github.com>
2024-11-17 12:30:37 +0800

    Update verifyJWT.js

--- src/middleware/verifyJWT.js
+++ src/middleware/verifyJWT.js
@@ -3,16 +3,17 @@ import User from "../../models/userModel.js";
 
 export function verifyJWT(req, res, next) {
   const authHeader = req.headers["authorization"];
-  if (!authHeader) return res.sendStatus(401);
+  if (!authHeader)
+    return res.status(401).json({ message: "credentials not found" });
 
   const token = authHeader && authHeader.split(" ")[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (error, payload) => {
-    if (error) return res.sendStatus(401);
+    if (error) return res.status(401).send({ message: "invalid credentials" });
 
     const user = await User.findById(payload.id);
 
-    if (!user) return res.sendStatus(401);
+    if (!user) return res.status(401).send({ message: "user not found" });
 
     req.user = user;
     next();
@@ -21,18 +22,19 @@ export function verifyJWT(req, res, next) {
 
 export function verifyJWTAdmin(req, res, next) {
   const authHeader = req.headers["authorization"];
-  if (!authHeader) return res.sendStatus(401);
+  if (!authHeader)
+    return res.status(401).json({ message: "credentials not found" });
 
   const token = authHeader && authHeader.split(" ")[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (error, payload) => {
-    if (error) return res.sendStatus(401);
+    if (error) return res.status(401).send({ message: "invalid credentials" });
 
     const user = await User.findById(payload.id);
 
-    if (!user) return res.sendStatus(401);
+    if (!user) return res.status(401).send({ message: "user not found" });
 
-    if (user.role !== "admin") return res.sendStatus(403);
+    if (user.role !== "admin") return res.status(403);
     req.user = user;
     next();
   });
